c1d1eac877414526e10f7ca1eacda6abf1289281
Jannylgm736 <janny.garcia703@gmail.com>
2025-03-25 15:49:27 -0600

    Html
    Cambios

--- scripts/controller_login/controller_login.js
+++ scripts/controller_login/controller_login.js
@@ -129,6 +129,7 @@ function validar_contraseña(){
         document.getElementById('reg-contraseña').style.borderColor = 'green';
     }
 
+<<<<<<< Updated upstream
     if (event.target.id === 'conf-contraseña' || event.target.id === 'reg-contraseña') {
         if (regcontraseña !== confcontraseña){
             errorMessage.style.display = 'block';
@@ -170,6 +171,19 @@ function calcularEdad(){
     }
 
     document.getElementById('edad').value = edad;
+=======
+    /* let minLength = regcontraseña.length >= 8;
+    let myletras = /[A-z]/.test(regcontraseña);
+    let mnletras= /[a-z]/.test(regcontraseña);
+    let numeros= /\d/.test(regcontraseña);
+    let ctrespeciales= /[()*#@]/.test(regcontraseña);
+    document.getElementById
+    document.getElementById
+    document.getElementById
+    document.getElementById
+    document.getElementById */
+
+>>>>>>> Stashed changes
 }
 
 /* function enter_enviar(event){
