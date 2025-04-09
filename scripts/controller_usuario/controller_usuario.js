31a0cd3a923aa2567c736d4f4671de73e7d8c9be
MAVP0509 <miguelvera3096@gmail.com>
2025-04-09 11:01:07 -0600

    Navegación
    Optimización en la navegación

--- scripts/controller_usuario/controller_usuario.js
+++ scripts/controller_usuario/controller_usuario.js
@@ -61,7 +61,9 @@ window.addEventListener('load', function () {
 
 let usuarios = []
 async function consultar_usuarios() {
-
+    let usuarioLog = JSON.parse(sessionStorage.getItem('user'))
+    let user = document.getElementById('user')
+    user.textContent = usuarioLog.resultado[0] 
     let r = await server_usuario({accion : 2})
 
     usuarios = r.resultado
