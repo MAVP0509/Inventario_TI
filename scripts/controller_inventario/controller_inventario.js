d84919dcad3599f47e1a58521884c2492d514fea
MAVP0509 <miguelvera3096@gmail.com>
2025-04-21 08:40:26 -0600

    prueba
    prueba

--- scripts/controller_inventario/controller_inventario.js
+++ scripts/controller_inventario/controller_inventario.js
@@ -242,6 +242,26 @@ async function consultar_informacion(params) {
                         confirmar_eliminacion();
                     }
                 },
+                {
+                    text: '<i class="fa-solid fa-trash fa-lg"></i> Eliminar registro',
+                    className: 'btn btn-sm btn-primary ',
+                    attr: {
+                        style: `
+                            background-color:rgb(211, 38, 38); 
+                            color: white; 
+                            border-radius: 50px; 
+                            padding: 10px 20px; 
+                            font-size: 16px; 
+                            font-weight: bold; 
+                            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+                            transition: all 0.3s ease;
+                        `, // Estilos inline
+                        title: 'Haz clic para eliminar un registro'
+                    },
+                    action: function (e, dt, node, config) {
+                        confirmar_eliminacion();
+                    }
+                },
             ],
             stateSave: true,
             resposive: true,
@@ -570,6 +590,3 @@ function descargar_excel() {
     return false; // Para evitar que el enlace navegue
 }
 
-function hola() {
-    console.log("olis")
-}
