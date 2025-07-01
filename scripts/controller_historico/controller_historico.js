d23acb5d76f4806c420756d53085482f1b93452b
MAVP0509 <miguelvera3096@gmail.com>
2025-07-01 13:29:51 -0600

    ajuste

--- scripts/controller_historico/controller_historico.js
+++ scripts/controller_historico/controller_historico.js
@@ -95,6 +95,7 @@ async function consultar_historico() {
         locale: "es",
         data: dato,
         pagination: true,
+        height: "800px",
         paginationSize: 15,
         paginationSizeSelector: [5, 10, 15, 25, 35],
         movableColumns: true,
