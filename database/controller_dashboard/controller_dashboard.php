944827bd934c53dc70c0edab729f23991d4ab85a
MAVP0509 <miguelvera3096@gmail.com>
2026-02-11 18:22:24 -0600

    Create controller_dashboard.php

---
+++ database/controller_dashboard/controller_dashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+date_default_timezone_set('America/Mexico_City');
+
+$clientejson = json_decode($_POST['trama']);
+
+$respuesta_servidor = new stdClass();
+
+if ($clientejson->accion == 0) {
+    $respuesta_servidor->resultado = consultar_datos($clientejson);
+}
+
+print(json_encode($respuesta_servidor));
+
+function consultar_datos($valores) {
+
+}
